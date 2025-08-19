4b383e5b1fa4c7c49e756f84e5502f7ff0b81ca4
Ana-Marija Drašković <ana.marija.draskovic007@gmail.com>
2025-08-19 12:03:30 +0000

    Initial JS functionality

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+let convertBtn = document.getElementById("convert-btn")
+let inputEl = document.getElementById("input")
+let lengthEl = document.getElementById("length-el")
+
+const meterToFeet =  3.281
+const literToGallon =  0.264
+const kiloToPound =  2.204
+
+convertBtn.addEventListener("click", function() {
+    let baseValue = inputEl.value
+    
+    lengthEl.textContent = `${baseValue} meter = ${baseValue * meterToFeet} feet` 
+    console.log(baseValue)
+})
