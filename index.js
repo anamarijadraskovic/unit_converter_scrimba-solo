0455146fec734c3023ee68516ed37c8567078d7c
Ana-Marija Drašković <ana.marija.draskovic007@gmail.com>
2025-08-29 12:26:44 +0000

    Fix form bug with prevent default

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ let lengthEl = document.getElementById("length-el")
 let volumeEl = document.getElementById("volume-el")
 let massEl = document.getElementById("mass-el")
 
-const meterToFeet =  3.281
-const literToGallon =  0.264
-const kiloToPound =  2.204
+const meterToFeet = 3.281
+const literToGallon = 0.264
+const kiloToPound = 2.204
 
 const conversions = [
     {
@@ -30,15 +30,16 @@ const conversions = [
     },
 ]
 
-function renderConversion (value, { factor, unitA, unitB, outputEl}) {
+function renderConversion(value, {factor, unitA, unitB, outputEl}) {
     const aToB = `${value} ${unitA}${value !== 1 ? "s" : ""} = ${(value * factor).toFixed(3)} ${unitB}${value != 1 ? "s" : ""}`;
-    const bToA = `${value} ${unitB}${value !==1 ? "s" : ""} = ${(value / factor).toFixed(3)} ${unitA}${value != 1 ? "s" : ""}`;
+    const bToA = `${value} ${unitB}${value !== 1 ? "s" : ""} = ${(value / factor).toFixed(3)} ${unitA}${value != 1 ? "s" : ""}`;
     outputEl.textContent = `${aToB} | ${bToA}`;
 }
 
-convertBtn.addEventListener("click", () => {
-  const baseValue = Number(inputEl.value) || 0;
-  conversions.forEach(conv => renderConversion(baseValue, conv));
+convertBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const baseValue = Number(inputEl.value) || 0;
+    conversions.forEach(conv => renderConversion(baseValue, conv));
 });
 
 // THEME SWITCHING
@@ -46,15 +47,15 @@ const toggleThemeBtn = document.getElementById("toggle-theme-btn");
 const root = document.documentElement;
 
 toggleThemeBtn.addEventListener("click", () => {
-  if (root.getAttribute("data-theme") === "dark") {
-    root.removeAttribute("data-theme");  // back to light
-    localStorage.setItem("theme", "light");
-  } else {
-    root.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  }
+    if (root.getAttribute("data-theme") === "dark") {
+        root.removeAttribute("data-theme");  // back to light
+        localStorage.setItem("theme", "light");
+    } else {
+        root.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+    }
 });
 
 if (localStorage.getItem("theme") === "dark") {
-  root.setAttribute("data-theme", "dark");
+    root.setAttribute("data-theme", "dark");
 }
